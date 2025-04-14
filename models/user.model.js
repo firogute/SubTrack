2c1213ec12560049079c26d036319de7fb772d29
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-04-14 05:16:15 +0300

    feat: add User model with validation and timestamps

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      minLength: 2,
+      maxLength: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "User name is required"],
+      unique: true,
+      minLength: 2,
+      maxLength: 255,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "User password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true },
+);
+const User = mongoose.model("User", userSchema);
+export default User;
